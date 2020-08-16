ee71c8026cece19a23ba5c09d8a4a85c51d77b74
Tejus Shinde <shindetejus@gmail.com>
2020-08-16 20:44:26 +0530

    Code written in the given R file. Contains 2 functions for making a cacheable matrix and a function to compute the inverse of such matrix if it doesn't exist.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix rather than compute it repeatedly.
+## 2 functions required. First : Makes a cacheableMatrix. Second : Computes inverse of
+## given matrix if it's not pre calculated.
 
-## Write a short comment describing this function
+## Following Fn gives a list of 4 functions
+'sets the value of the matrix
+gets the value of the matrix
+sets the value of the inverse
+gets the value of the inverse'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMat <- NULL
+    
+    # sets the value of the matrix
+    set <- function(y) {
+        x <<- y
+        inverseMat <- NULL
+    }
+    
+    # gets the value of the matrix
+    get <- function() x
+    
+    # sets the value of the inverse
+    setInverse <- function(Inverse = matrix()) {
+        inverseMat <<- Inverse
+    }
+    
+    # gets the value of the inverse
+    getInverse <- function() {inverseMat
+        }
+    list(set= set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Function computing the matrix Inverse if it's not pre-calculated
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    InverseM <- x$getInverse()
+    if(!is.null(InverseM)) {
+        print("Showing cached Inverse of the Matrix")
+        InverseM
+    } else {
+        Data <- x$get()
+        InverseM <- solve(Data)
+        x$setInverse(InverseM)
+        InverseM
+    }
 }
+
+X <- matrix(rnorm(100,2,10), 10,10) # Normal matrix initialization
+Mat <- makeCacheMatrix(X)           # Converting that matrix into a cached Matrix
+
+# computing the Inverse of our special matrix here
+cacheSolve(Mat)
+
+
+
